f4c7e9ed60a0b5e110b5279f45d583c161a8fd74
dannymoon <moon.hojik@gmail.com>
2017-11-14 21:56:59 -0800

    done

---
+++ BeastList/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  BeastList
+//
+//  Created by Danny Moon on 11/6/17.
+//  Copyright © 2017 Danny Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+
+    var tasks = [""]
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        tasks.append(taskTextField.text!)
+        tableView.reloadData()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section:\(indexPath.section) and Row:\(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+            
+}
+
